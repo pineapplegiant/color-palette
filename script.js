fbe1948f507d4f81df1331629b2267c67cba4c61
xeare <teshome0@gmail.com>
2019-09-27 00:37:14 -0600

    more styling. functionality for colorfilling in prog

--- script.js
+++ script.js
@@ -1,47 +1,52 @@
-/* eslint-disable require-jsdoc */
 const colorPickerContainer = document.getElementsByClassName("container");
 const colorPalleteContainer = document.getElementsByClassName("pallete");
-const theWholePage = document.getElementsByClassName("whole-page");
+const page = document.getElementById("whole-page");
 
-function listen() {
-  document
-    .querySelector(".whole-page")
-    .addEventListener("click", function(event) {
-      if (event.target.tagName === "BUTTON") {
-        if (event.target.innerText === "Add New Color") {
-          
-          addNewColorPicker();
-        } else if (event.target.innerText === "Delete") {
-          deleteColorPicker(event.target.id);
-        }
-      }
-    });
+page.addEventListener("click", buttonHandler, false);
+page.addEventListener("change", colorHandler, false);
+
+function buttonHandler(event) {
+  if (event.target.tagName === "BUTTON") {
+    if (event.target.innerText === "Add New Color") {
+      addNewColorPicker();
+    } else if (event.target.innerText === "x") {
+      deleteColorPicker(event.target.id);
+    } else if (event.target.innerText === "Start Over") {
+      startOver();
+    }
+  }
+}
+
+function colorHandler(event) {
+  
+  if (event.target.className === "second-input") {
+
+    fillInColor(event.target.value, event.target.id);
+  }
 }
 
 function addNewColorPicker() {
-  colorNumber = howManyColors();
-  randomId = Math.random() // this is next level right here
+  colorNumber = colorCounter();
+  randomId = Math.random(); // this is next level right here
   let colorBox = document.createElement("div");
   colorBox.className = "color-box";
   colorBox.id = randomId; // to use when listening to color changes in picker to display in pallete
 
   let h2 = document.createElement("h2");
-
-  h2.innerText = `Color {${colorNumber}}`;
+  h2.innerText = colorNumber;
+  h2.className = "h2";
 
   let firstInput = document.createElement("input");
   firstInput.type = "color";
   firstInput.className = "color";
-  firstInput.name = "y";
 
   let secondInput = document.createElement("input");
-  secondInput.type = "tel";
-  secondInput.id = "";
-  secondInput.name = "";
+  secondInput.className = 'second-input';
+  secondInput.id = randomId;
 
   let deleteButton = document.createElement("button");
   deleteButton.className = "delete-button";
-  deleteButton.innerText = "Delete";
+  deleteButton.innerText = "x";
   deleteButton.id = randomId;
 
   colorBox.appendChild(h2);
@@ -51,17 +56,26 @@ function addNewColorPicker() {
 
   // page is an iterable for some reason, a node i think, so we gotta iterate over it
   colorPickerContainer[0].appendChild(colorBox);
-  addDisplayColumn(randomId); // every color picker needs their color displayed
+  addDisplayColumn(randomId, colorNumber); // every color picker needs their color displayed
 }
 
-function howManyColors() {
+function colorCounter() {
   let numberOfColorBoxes = document.getElementsByClassName("color-box").length;
   return numberOfColorBoxes + 1; // js arrays start at 0 ^_^ and no on cares for color {0}
 }
-function addDisplayColumn(colorNumber) {
+
+function addDisplayColumn(randomId, colorNumber) {
   let displayColumn = document.createElement("div");
   displayColumn.className = "color-column";
-  displayColumn.id = colorNumber; // same as colorBox.id to tie them together
+  displayColumn.tagName = randomId;
+
+  displayColumn.id = randomId; // same as colorBox.id to tie them together
+
+  let h2 = document.createElement("h2");
+  h2.innerText = colorNumber;
+  h2.className = "h2";
+
+  displayColumn.appendChild(h2);
   colorPalleteContainer[0].appendChild(displayColumn);
 }
 
@@ -72,4 +86,15 @@ function deleteColorPicker(idNumber) {
   displayColumnToBeDeleted.remove();
 }
 
-listen();
+function startOver() {
+  // second chances for everyone and maybe thirds and ...
+  location.reload();
+}
+
+function fillInColor(colorValue, idNumber) {
+  // wip
+  console.log(document.getElementsByTagName(idNumber))
+  console.log(idNumber);
+}
+
+// columnDisplay.style.backgroundColor = colorValue;
